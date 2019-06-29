c21dc016530301ecd238c754bd86ded530885427
cnobre <carolinanobre@gmail.com>
2019-06-29 13:22:44 -0600

    added encoding for categorical attributes with node border color. selected nodes are now filled in with that same color to avoid the use of yet one more color to encode 'selected'

--- experiments/node-link/public/js/main_nodeLink.js
+++ experiments/node-link/public/js/main_nodeLink.js
@@ -369,6 +369,17 @@ function drawVis(root) {
     .attr("orient", "auto")
   .append("svg:path")
     .attr("d", "M0,-5L10,0L0,5");
+
+    // svg.append("symbol")
+    // .attr('class','svg-icon')
+    // .attr('width','2em')
+    // .attr('heigh','2em')
+    // .attr('id','personIcon')
+    // .attr('viewBox','0 0 20 20')
+    // .append('path')
+    // .attr('d',"M12.075,10.812c1.358-0.853,2.242-2.507,2.242-4.037c0-2.181-1.795-4.618-4.198-4.618S5.921,4.594,5.921,6.775c0,1.53,0.884,3.185,2.242,4.037c-3.222,0.865-5.6,3.807-5.6,7.298c0,0.23,0.189,0.42,0.42,0.42h14.273c0.23,0,0.42-0.189,0.42-0.42C17.676,14.619,15.297,11.677,12.075,10.812 M6.761,6.775c0-2.162,1.773-3.778,3.358-3.778s3.359,1.616,3.359,3.778c0,2.162-1.774,3.778-3.359,3.778S6.761,8.937,6.761,6.775 M3.415,17.69c0.218-3.51,3.142-6.297,6.704-6.297c3.562,0,6.486,2.787,6.705,6.297H3.415z")
+
+
     
     //set up svg and groups for nodes/links
     svg
@@ -568,7 +579,12 @@ function drawVis(root) {
         .attr("dy", "-2em")
         .classed("label", true)
 
+        // nodeEnter
+        // .append('use')
+        // .attr('xlink:href', "#personIcon")
 
+
+        
         node.exit().remove();
 
         node = nodeEnter.merge(node);
@@ -577,7 +593,7 @@ function drawVis(root) {
         //   .attr("fill", d => nodeColor(d.type));
 
           node.select('.node')
-          .attr("fill", 'none');
+          .attr('class',d=>'node ' + d.type)
 
         node.selectAll('.frame')
           .attr("height", friends_scale.range()[1])
